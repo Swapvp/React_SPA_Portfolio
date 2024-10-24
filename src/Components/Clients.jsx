59d61474e0291591e5f27091264aaaa722eb2b7c
Swapvp <swapneshpawar1994@gmail.com>
2024-10-24 21:43:19 +0530

    client-page

--- src/Components/Clients.jsx
+++ src/Components/Clients.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import clientLogo from "../assets/c7.png";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className="padding">
+      <div className="secTitle">
+        <h2 className="text-center text-uppercase">Clients</h2>
+      </div>
+      <div className="container">
+        <div className="row justify-content-center align-items-center my-5  ">
+          <div className="col-md-2">
+            <img src={clientLogo} alt="" />
+          </div>
+          <div className="col-md-2">
+            <img src={clientLogo} alt="" />
+          </div>
+          <div className="col-md-2">
+            <img src={clientLogo} alt="" />
+          </div>
+          <div className="col-md-2">
+            <img src={clientLogo} alt="" />
+          </div>
+          <div className="col-md-2">
+            <img src={clientLogo} alt="" />
+          </div>
+          <div className="col-md-2">
+            <img src={clientLogo} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
