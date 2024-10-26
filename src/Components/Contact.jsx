e3e87c1ba58fa7e0de4305c0bf8e798f9187245d
Swapvp <swapneshpawar1994@gmail.com>
2024-10-26 17:16:29 +0530

    portfolio=contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,57 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="padding">
+      <div className="secTitle">
+        <h2 className="text-center text-uppercase">Contact</h2>
+      </div>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-6">
+            <form action="#">
+              <div className="row">
+                <div className="col-md-6">
+                  <input
+                    type="text"
+                    name="fname"
+                    id="fname"
+                    placeholder="First Name"
+                    className="w-100"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <input
+                    type="email"
+                    name="email"
+                    id=""
+                    placeholder="Your Email"
+                    className="w-100"
+                  />
+                </div>
+              </div>
+              <div className="my-2">
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Subject"
+                  className="w-100"
+                />
+              </div>
+              <div className="my-2">
+                <textarea name="" id="" className="w-100"></textarea>
+              </div>
+              <div className="my-2">
+                <input type="submit" value="SUBMIT" className="w-100" />
+              </div>
+            </form>
+          </div>
+          <div className="col-md-6"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import p1 from "../assets/portfolio/p1.jpg";
+import p2 from "../assets/portfolio/p2.jpg";
+import p3 from "../assets/portfolio/p3.jpg";
+import p4 from "../assets/portfolio/p4.jpg";
+import p5 from "../assets/portfolio/p5.jpg";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section className="padding">
+      <div className="secTitle">
+        <h2 className="text-center text-uppercase">Portfolio</h2>
+      </div>
+      <div className="container my-5">
+        <div className="row justify-content-center text-center">
+          <div className="col-md-4 text-center">
+            <div>
+              <img src={p1} alt="" />
+            </div>
+            <div className="my-4">
+              <img src={p2} alt="" />
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <div>
+              <img src={p3} alt="" />
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <div>
+              <img src={p4} alt="" />
+            </div>
+            <div className="my-4">
+              <img src={p5} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
